0ee2e33ddd1b0786de6389624caf936e6a2e6c9c
Eason Pai <epai@adobe.com>
2025-11-07 15:43:06 +0800

    force to update CORS

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -12,6 +12,7 @@ import {
 import { decorateRichtext } from './editor-support-rte.js';
 import { decorateMain } from './scripts.js';
 
+
 async function applyChanges(event) {
   // redecorate default content and blocks on patches (in the properties rail)
   const { detail } = event;
