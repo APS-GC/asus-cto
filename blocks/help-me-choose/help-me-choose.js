b01ab548381123851933a806eef95ce915305018
Rajesh Pal <tec28425@adobe.com>
2025-11-13 10:17:52 +0530

    5800 [Enable - Help Me Choose Block]

---
+++ blocks/help-me-choose/help-me-choose.js
@@ -0,0 +1,9 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+
+export default function decorate(block) { 
+
+    console.log('Hello >>> help-me-choose block');
+
+}
