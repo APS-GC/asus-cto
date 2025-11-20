2742b294b67483d9973de42ff63d040fcbb77afb
Yuki <455527653@qq.com>
2025-11-20 10:29:45 +0800

    add our advantages block

--- blocks/our-advantages/our-advantages.js
+++ blocks/our-advantages/our-advantages.js
@@ -1,85 +1,60 @@
-import {isAuthorEnvironment, safeText} from '../../scripts/utils.js';
-import {moveInstrumentation} from "../../scripts/scripts.js";
+import { isAuthorEnvironment, safeText } from "../../scripts/utils.js";
+import { transferInstrumentation } from "../../scripts/utils.js";
 
+const itemsStartIndex = 3;
 export default async function decorate(block) {
-
-    await renderBlock(block);
-
-    await import('./uifrontend_carousel.js');
-    await import('./uifrontend_advantage-card.js');
-
-    document.dispatchEvent(new Event('asus-cto-our-advantages'));
-}
-
-async function renderBlock(block) {
-
-    const mockupHeading = document.createRange().createContextualFragment(`<div class="section-heading content-center">
-        <h2 data-richtext-prop="text" data-richtext-label="Text" data-richtext-filter="text">${block.firstElementChild.textContent.trim()}</h2>
-      </div>`);
-
-    if (isAuthorEnvironment()) {
-        moveInstrumentation(block.firstElementChild.firstElementChild, mockupHeading.firstElementChild);
-    }
-
-    const mockupContainer = document.createRange().createContextualFragment(`<div class='container'>
+  const divs = block.children;
+  const title = divs[0].textContent.trim() || "Our Advantages";
+  const itemCount = Number(divs[1].textContent.trim()) || 3;
+  const imageAutoplayDuration = Number(divs[2].textContent.trim()) || 5;
+  const mockupContainer = document.createRange()
+    .createContextualFragment(`<div class='container'>
     <div class="carousel panelcontainer">
+      <div class="section-heading content-center">
+        <h2>${title}</h2>
+      </div>
       <div
-          id="carousel-4e80c7e13a"
-          class="cmp-carousel"
-          role="group"
-          aria-live="polite"
-          aria-roledescription="carousel"
-          data-cmp-is="carousel"
-          data-cmp-delay="5000"
-          data-carousel-effect="creative"
-          data-prev-slide-message="Previous advantage"
-          data-next-slide-message="Next advantage"
-        >
+        class="cmp-carousel"
+        role="group"
+        aria-live="polite"
+        aria-roledescription="carousel"
+        data-cmp-is="carousel"
+        data-cmp-delay="${imageAutoplayDuration*1000}"
+        data-carousel-effect="creative"
+      >
         <div class="cmp-carousel__content">
         </div>
 
         <!-- Carousel actions - Previous/Next -->
-          <div class="cmp-carousel__actions">
-            <button class="cmp-carousel__action cmp-carousel__action--previous">
-              <span class="visually-hidden"></span>
-            </button>
-            <button class="cmp-carousel__action cmp-carousel__action--next">
-              <span class="visually-hidden"></span>
-            </button>
-          </div>
+        <div class="cmp-carousel__actions">
+          <button class="cmp-carousel__action cmp-carousel__action--previous" type="button" aria-label="Previous">
+            <span class="visually-hidden"></span>
+          </button>
+          <button class="cmp-carousel__action cmp-carousel__action--next" type="button" aria-label="Next">
+            <span class="visually-hidden"></span>
+          </button>
+        </div>
 
-          <!-- Pagination indicatoors -->
-          <ol class="cmp-carousel__indicators" role="tablist" aria-label="Choose a slide to display"></ol>
+        <!-- Pagination indicatoors -->
+        <ol class="cmp-carousel__indicators" role="tablist" aria-label="Choose a slide to display"></ol>
       </div>
     </div>
   </div>`);
 
-    mockupContainer
-        .querySelector('.carousel.panelcontainer')
-        .prepend(mockupHeading);
+  const cardNodes = [];
+  [...block.children].forEach((card, i) => {
+    if (i > itemsStartIndex + itemCount - 1 || i < itemsStartIndex) return;
+    const divs = card.querySelectorAll("div");
+    const headline = safeText(divs.item(1));
+    const details = safeText(divs.item(2));
+    const navigate = safeText(divs.item(3));
+    const mediaHTML = card.querySelector("picture")?.innerHTML ?? "";
 
-
-    const cardNodes = [];
-    [...block.children].forEach((card, i) => {
-        // skip the heading
-        if (i === 0) return;
-        const divs = card.querySelectorAll('div');
-        const headline = safeText(divs.item(1));
-        const details = safeText(divs.item(2));
-        const navigate = safeText(divs.item(3));
-        const imgSrc = card ? card.querySelector('div > picture > img').getAttribute('src') : null;
-
-
-      const mockup = document.createRange().createContextualFragment(`
+    const mockup = document.createRange().createContextualFragment(`
           <div class="cmp-carousel__item">
             <div class="cmp-advantage-card">
               <div class="cmp-advantage-card__image-wrapper">
-                <img
-                  src="${imgSrc}"
-                  alt="Ultimate Gaming Experience"
-                  class="cmp-advantage-card__image"
-                  loading="lazy"
-                />
+                ${mediaHTML}
                 <video class="cmp-advantage-card__video" playsinline controls>
                   <source
                     type="video/mp4">
@@ -93,21 +68,39 @@ async function renderBlock(block) {
                     ${details}
                   </p>
                   <button class="cmp-advantage-card__btn btn">${navigate}<img
-                  alt="play-icon" /></button>
+                  alt="play-icon" src="/content/dam/eds-enablement-xwalk/asus-cto-sites/icon-play-filled.svg" /></button>
                 </div>
               </div>
             </div>
           </div>`);
 
-        if (isAuthorEnvironment()) {
-            moveInstrumentation(card, mockup.firstElementChild);
-        }
-        cardNodes.push(mockup);
-    });
-
-    mockupContainer.querySelector('.cmp-carousel__content').append(...cardNodes);
     if (isAuthorEnvironment()) {
-        moveInstrumentation(block, mockupContainer.firstElementChild);
+      transferInstrumentation(card, mockup);
     }
-    block.replaceWith(mockupContainer);
+    cardNodes.push(mockup);
+  });
+
+  mockupContainer.querySelector(".cmp-carousel__content").append(...cardNodes);
+
+  //move attr
+  if (isAuthorEnvironment()) {
+    //move title
+    if (block.firstElementChild) {
+      transferInstrumentation(
+        block.firstElementChild,
+        mockupContainer.querySelector(".section-heading")
+      );
+    }
+  }
+
+  block.innerHTML = "";
+  block.append(mockupContainer);
+
+  // trigger block
+  await import("../../scripts/carousel.js");
+  await import("./uifrontend_advantage-card.js");
+
+  if (window.initializeSwiperOnAEMCarousel) {
+    window.initializeSwiperOnAEMCarousel(block.querySelector(".container"));
+  }
 }
