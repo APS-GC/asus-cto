e5ebf6d2198325ebe31705b07d7ec15e551fe808
Rajesh Pal <tec28425@adobe.com>
2025-11-20 14:03:53 +0530

    [5800] Endpoint fixes

--- blocks/product-comparison/product-comparison.js
+++ blocks/product-comparison/product-comparison.js
@@ -1,4 +1,5 @@
 export default function decorate(block) {
+  console.log("Hello 1")
   const [quoteWrapper] = block.children;
   const blockquote = document.createElement('blockquote');
   blockquote.textContent = quoteWrapper.textContent.trim();
