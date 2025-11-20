ed415229e5d80a004713f5e76e804ead8e5463c9
Rajesh Pal <tec28425@adobe.com>
2025-11-20 14:16:19 +0530

    [5969] Product Compare

--- blocks/product-comparison/product-comparison.js
+++ blocks/product-comparison/product-comparison.js
@@ -1,7 +1,10 @@
+import { loadScript } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
 export default function decorate(block) {
   console.log("Hello 1")
-  const [quoteWrapper] = block.children;
-  const blockquote = document.createElement('blockquote');
-  blockquote.textContent = quoteWrapper.textContent.trim();
-  quoteWrapper.replaceChildren(blockquote);
+  // const [quoteWrapper] = block.children;
+  // const blockquote = document.createElement('blockquote');
+  // blockquote.textContent = quoteWrapper.textContent.trim();
+  // quoteWrapper.replaceChildren(blockquote);
 }
