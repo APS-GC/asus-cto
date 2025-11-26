2b90d55d4e0e6e30159b5da04e954fe58785f5ff
Rajesh Pal <tec28425@adobe.com>
2025-11-26 22:42:58 +0530

    Help me choose - issue fix

--- blocks/help-me-choose/help-me-choose.js
+++ blocks/help-me-choose/help-me-choose.js
@@ -54,7 +54,7 @@ async function renderHelpMeChoose(block) {
   // Build the HTML in a fragment / string, then insert once
   const html = 
   
-  !_isHomePage() ? `
+  AuthoredData[6] === "1" ? `
   <div class="game-recommendation">
       <div class="carousel panelcontainer">
           <div class="section-heading">
